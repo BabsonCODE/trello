4ab45f07f995bf7a111328f7d31f005a44e822a1
Eagle Wu <eaglewu014@gmail.com>
2016-06-12 15:53:34 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@
 
 window.horello = window.horello || {};
 
-horello.apiKey = "ab3e895cad67a947ad0fcc93d33fc689";
-horello.apiToken = "3f8ac1459d8885b64edf0259a2af87b4eb6f964599cd19143a378479dd2796d5";
+horello.apiKey = "not4ureyes";
+horello.apiToken = "not4ureyes";
 horello.apiUrl = "https://api.trello.com/1";
